148213dcfe7c8438cd962af773b5e830ebd3dba7
blue892 <110934835+blue892@users.noreply.github.com>
2024-03-22 13:33:22 -0400

    Add basic calculator layout to index.html

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+
+let firstNum;
+let secondNum;
+let operator;
+
+const operate = () => {
+
+}
